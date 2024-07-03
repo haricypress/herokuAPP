028af15b356d89c8d950e032e485c298d1fbc9f8
haricypress <pirate.evil.war@gmail.com>
2024-07-04 00:53:11 +0530

    completed

--- cypress/e2e/herokuAPP.cy.js
+++ cypress/e2e/herokuAPP.cy.js
@@ -1,6 +1,28 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://testpages.herokuapp.com/styled/tag/dynamic-table.html')
 
+describe('verify table data', () => {
+  var jsonData
+  before("", () => {
+
+    //loading test data from json file
+    cy.fixture("testData").then($data => {
+      jsonData = $data
+    })
+  })
+  it('verify test data with table data', () => {
+    
+    //visit webPage
+    cy.visit('https://testpages.herokuapp.com/styled/tag/dynamic-table.html')
+    cy.contains("Table Data").click()
+    cy.get("#jsondata").clear().type(JSON.stringify(jsonData), { parseSpecialCharSequences: false })
+    cy.get("#refreshtable").click()
+
+    //assert JSON test data with table data
+    jsonData.forEach(data => {
+      cy.get('#dynamictable').contains(data.name).should("be.visible")
+      cy.get('#dynamictable').contains(data.age).should("be.visible")
+      cy.get('#dynamictable').contains(data.gender).should("be.visible")
+
+
+    })
   })
 })
